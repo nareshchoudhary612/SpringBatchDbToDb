27d47646379fd68fd3899920bcc402d480e8bd67
nareshchoudhary612 <nareshchoudhary612@gmail.com>
2019-02-22 12:31:08 -0600

    added jpa reader

---
+++ src/test/java/springbatch_example/SpringBatchXmlToDatabase/SpringBatchXmlToDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package springbatch_example.SpringBatchXmlToDatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchXmlToDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
